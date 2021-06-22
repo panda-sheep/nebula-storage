21e72d0a785e18d110e3720cb87df18d72eee886
panda-sheep <59197347+panda-sheep@users.noreply.github.com>
2021-06-22 12:29:25 +0800

    use local id in the space range for tag edge index

--- src/meta/MetaServiceUtils.h
+++ src/meta/MetaServiceUtils.h
@@ -369,6 +369,10 @@ public:
         const std::unordered_set<GraphSpaceID>& spaces,
         const std::string& backupName,
         const std::vector<std::string>* spaceName);
+
+     static std::string localIdKey(GraphSpaceID spaceId);
+
+     static GraphSpaceID parseLocalIdSpace(folly::StringPiece rawData);
 };
 
 }   // namespace meta
